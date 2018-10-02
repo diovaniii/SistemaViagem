ca4d7b77a317c05bd6d770d3f7040a8e10888df7
Diovane Pereira <diovani.joao@gmail.com>
2018-10-02 19:06:00 -0300

    onibus

--- ViagemWeb/Site.Master.designer.cs
+++ ViagemWeb/Site.Master.designer.cs
@@ -13,6 +13,15 @@ namespace ViagemWeb {
     public partial class SiteMaster {
         
         /// <summary>
+        /// WebUserControl1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.UserControl WebUserControl1;
+        
+        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
